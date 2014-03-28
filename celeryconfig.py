24088d8d1a2a2ca494af2eeceb4cbac7b127b276
sardor <sardor@hanbiro.com>
2014-03-28 20:36:08 +0900

    Redis db number hardcode

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,3 +1,3 @@
-BROKER_URL = 'redis://:gksqlfhWkd!!@localhost:6379/0' 
-CELERY_RESULT_BACKEND = 'redis://:gksqlfhWkd!!@localhost:6379/1'
+BROKER_URL = 'redis://:gksqlfhWkd!!@localhost:6379/2014' 
+CELERY_RESULT_BACKEND = 'redis://:gksqlfhWkd!!@localhost:6379/2015'
 CELERY_ACCEPT_CONTENT = ['json']
