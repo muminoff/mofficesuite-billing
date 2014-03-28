c867476c72596656da885f5779491fc98e975ba8
sardor <sardor@hanbiro.com>
2014-03-28 20:34:49 +0900

    Redis password hardcode

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,3 +1,3 @@
-BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/1'
+BROKER_URL = 'redis://:gksqlfhWkd!!@localhost:6379/0' 
+CELERY_RESULT_BACKEND = 'redis://:gksqlfhWkd!!@localhost:6379/1'
 CELERY_ACCEPT_CONTENT = ['json']
