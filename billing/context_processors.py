dce74aabd8e6829d952bebb7cbea1a29b6f4b210
sardor <sardor@hanbiro.com>
2014-03-28 20:12:18 +0900

    Context sidebar anonymous user fixed

--- billing/context_processors.py
+++ billing/context_processors.py
@@ -4,26 +4,15 @@ from django.utils.translation import ugettext_lazy as _
 
 
 def sidebar(request):
-    if request.user.is_authenticated and request.user.is_crew:
-        menus = OrderedDict([
-            ('services_page', {'title': 'Services', 'icon': 'cloud'}),
-            ('account_page', {'title': 'Account', 'icon': 'profile-male'}),
-            ('billing_page', {'title': 'Billing', 'icon': 'wallet'}),
-            # ('support_page', {'title': 'Support', 'icon': 'support'}),
-            ('security_page', {'title': 'Security', 'icon': 'lock'}),
-            ('notifications_page', {'title': 'Notifications', 'icon': 'megaphone'}),
-            ('settings_page', {'title': 'Settings', 'icon': 'adjustments'}),
-            ])
-    else:
-        menus = OrderedDict([
-            ('services_page', {'title': 'Services', 'icon': 'cloud'}),
-            ('account_page', {'title': 'Account', 'icon': 'profile-male'}),
-            ('billing_page', {'title': 'Billing', 'icon': 'wallet'}),
-            # ('support_page', {'title': 'Support', 'icon': 'support'}),
-            ('security_page', {'title': 'Security', 'icon': 'lock'}),
-            ('notifications_page', {'title': 'Notifications', 'icon': 'megaphone'}),
-            ('settings_page', {'title': 'Settings', 'icon': 'adjustments'}),
-            ])
+    menus = OrderedDict([
+        ('services_page', {'title': 'Services', 'icon': 'cloud'}),
+        ('account_page', {'title': 'Account', 'icon': 'profile-male'}),
+        ('billing_page', {'title': 'Billing', 'icon': 'wallet'}),
+        # ('support_page', {'title': 'Support', 'icon': 'support'}),
+        ('security_page', {'title': 'Security', 'icon': 'lock'}),
+        ('notifications_page', {'title': 'Notifications', 'icon': 'megaphone'}),
+        ('settings_page', {'title': 'Settings', 'icon': 'adjustments'}),
+        ])
 
 
     try: 
